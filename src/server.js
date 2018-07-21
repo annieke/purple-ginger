6637860a22e211ccab0e9c55590cf9b77b6dd3ba
annie ke <annieke8@gmail.com>
2018-07-21 09:12:00 -0700

    bot replies to hi

--- src/server.js
+++ src/server.js
@@ -3,6 +3,10 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import path from 'path';
 import morgan from 'morgan';
+import botkit from 'botkit';
+import dotenv from 'dotenv';
+
+dotenv.config({ silent: true });
 
 // initialize
 const app = express();
@@ -26,7 +30,6 @@ app.set('views', path.join(__dirname, '../src/views'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
 // default index route
 app.get('/', (req, res) => {
   res.send('hi');
@@ -38,3 +41,46 @@ const port = process.env.PORT || 9090;
 app.listen(port);
 
 console.log(`listening on: ${port}`);
+
+// botkit controller
+const controller = botkit.slackbot({
+  debug: false,
+});
+
+// initialize slackbot
+const slackbot = controller
+  .spawn({
+    token: process.env.SLACK_BOT_TOKEN,
+    // this grabs the slack token we exported earlier
+  })
+  .startRTM((err) => {
+    // start the real time message client
+    if (err) {
+      throw new Error(err);
+    }
+  });
+
+// prepare webhook
+// for now we won't use this but feel free to look up slack webhooks
+controller.setupWebserver(process.env.PORT || 3001, (err, webserver) => {
+  controller.createWebhookEndpoints(webserver, slackbot, () => {
+    if (err) {
+      throw new Error(err);
+    }
+  });
+});
+
+// example hello response
+controller.hears(
+  ['hello', 'hi', 'howdy'],
+  ['direct_message', 'direct_mention', 'mention'],
+  (bot, message) => {
+    bot.api.users.info({ user: message.user }, (err, res) => {
+      if (res) {
+        bot.reply(message, `Hello, ${res.user.name}!`);
+      } else {
+        bot.reply(message, 'Hello there!');
+      }
+    });
+  },
+);
