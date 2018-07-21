97b4f749440ae75003be2352581b66d32e52b9fd
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2018-07-21 10:04:04 -0700

    created db connection file and user actions file

---
+++ src/db.js
@@ -0,0 +1,13 @@
+
+import mongoose from 'mongoose';
+
+const database = 'mongodb://127.0.0.1/gingy';
+mongoose.connect(database, (err, res) => {
+  if (err) {
+    console.log('There was an error connecting the database');
+  } else {
+    console.log('Connected to the database!');
+  }
+});
+
+module.exports = mongoose.connection;
